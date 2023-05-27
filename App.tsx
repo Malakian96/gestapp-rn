463d5839319f64a42744e29140dffeb2ff800ed0
origarcies <origarcies@gmail.com>
2023-05-27 18:33:17 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {Text,useColorScheme,} from 'react-native';
+
+import {Colors,} from 'react-native/Libraries/NewAppScreen';
+
+const App = (): JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <Text> pene </Text> 
+  );
+}
+
+export default App;
